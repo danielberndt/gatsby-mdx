e564a57a38d480bdc40f782aae3ad118c55fba02
Daniel Berndt <daniel.berndt@gmail.com>
2018-10-10 16:59:46 +0200

    use new html-to-jsx

--- packages/gatsby-mdx/utils/get-source-plugins-as-remark-plugins.js
+++ packages/gatsby-mdx/utils/get-source-plugins-as-remark-plugins.js
@@ -1,6 +1,6 @@
 const visit = require("unist-util-visit");
 const _ = require("lodash");
-const HTMLtoJSX = require("htmltojsx");
+const HTMLtoJSX = require("@erikwithuhk/html-to-jsx");
 const map = require("unist-util-map");
 const debug = require("debug")("get-source-plugins-as-remark-plugins");
 
